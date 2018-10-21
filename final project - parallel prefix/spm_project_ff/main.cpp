42ab93f787635d39829c9140cc5bbd6bfd9c5caa
Alessandra <alessandra.fais@gmail.com>
2018-10-21 19:24:39 +0200

    Added files FastFlow version

---
+++ final project - parallel prefix/spm_project_ff/main.cpp
@@ -0,0 +1,277 @@
+//
+// Created by Alessandra Fais
+// SPM project - MCSN - University of Pisa
+// A.A. 2017/18
+//
+
+#include <algorithm>
+#include <random>
+#include <string>
+
+#include "scan.h"
+
+const int add_op_id = 0;
+const int mul_op_id = 1;
+const std::string str_conc_id = "";
+
+const int comp_delay = 10; // millisecs
+
+int add (int a, int b);
+int mul (int a, int b);
+std::string str_concat (std::string a, std::string b);
+
+std::vector<int> generateIntVector(size_t size, unsigned int seed);
+std::vector<std::string> generateStringVector(size_t size, unsigned int seed);
+void active_delay(int msecs);
+
+int main(int argc, char* argv[]) {
+
+    // ------------------------------------- INPUT PARAMETERS -------------------------------------
+
+    if (argc < 4) {
+        std::cout << "Parallel Scan usage:\n"
+                  << argv[0]
+                  << " <vec_size> <type (e = exclusive, i = inclusive)> <nw> <seed> <test>\n"
+                  << "Choose a test inserting the corresponding number:\n"
+                  << "1. parallel prefix on a random vector of integers, associative operation is the addition\n"
+                  << "2. parallel prefix on a random vector of integers, associative operation is the multiplication\n"
+                  << "3. parallel prefix on a random vector of strings, associative operation is the concatenation\n";
+        return 1;
+    }
+
+    size_t vec_size;
+    std::istringstream is1(argv[1]);
+    if (!(is1 >> vec_size) || vec_size < 1) {
+        std::cerr << "Invalid vector size." << std::endl;
+        return 1;
+    }
+
+    scan_type scan_t;
+    switch (argv[2][0]) {
+        case 'e':
+            scan_t = EXCLUSIVE;
+            break;
+        default:
+            scan_t = INCLUSIVE;
+            break;
+    }
+
+    int nw;
+    std::istringstream is2(argv[3]);
+    if (!(is2 >> nw) || nw < 0) {
+        std::cerr << "Invalid parallelism degree." << std::endl;
+        return 1;
+    }
+
+    unsigned int seed;
+    if (argc == 4) {
+        std::random_device random_device;
+        seed = random_device();
+    } else {
+        std::istringstream is3(argv[4]);
+        if (!(is3 >> seed)) {
+            std::cerr << "Invalid seed." << std::endl;
+            return 1;
+        }
+    }
+
+    int test = 1;
+    if (argc == 6) {
+        std::istringstream is4(argv[5]);
+        if (!(is4 >> test) || test < 0 || test > 3) {
+            std::cerr << "Invalid test number." << std::endl;
+            return 1;
+        }
+    }
+
+    std::cout << "Preparing execution of test number " << test
+              << " with parameters size=" << vec_size
+              << " type=" << scan_t
+              << " nw=" << nw
+              << " seed=" << seed << std::endl;
+
+    if (test == 1) {
+
+        // Measure the addition time.
+        auto ta = std::chrono::high_resolution_clock::now();
+        int a = 10, b = 100;
+        int res = a + b;
+        auto tb = std::chrono::high_resolution_clock::now();
+        auto op_time = std::chrono::duration_cast<std::chrono::milliseconds>(tb - ta).count(); // ~ 0 microsecs
+
+        // Time required for sequential execution.
+        std::cerr << "Expected time: Tseq = "
+                  << (vec_size - 1) * (op_time + comp_delay);
+
+        if (nw > 0) {
+            // Time required for parallel execution: ~ (2 * vec_size/nw * (op_time + comp_delay)) since O(nw) term is negligible
+            std::cerr << " ms  ,  Tpar(" << nw << ") = "
+                      << (((vec_size / nw - 1) * (op_time + comp_delay)) +      // reduce in workers -> O(vec_size/nw)
+                          ((nw - 1) * (op_time + comp_delay)) +                 // exclusive scan in distributor -> O(nw)
+                          ((vec_size / nw - 1) * (op_time + comp_delay)))       // scan in workers -> O(vec_size/nw)
+                      << " ms     Real time: ";
+        } else std::cerr << " ms     Real time: ";
+
+        // ------------------------------------------ TEST ADD ------------------------------------------
+        std::vector<int> in1 = generateIntVector(vec_size, seed);
+        std::function<int(int, int)> add_op = add;
+
+        Scan<int> my_scan1(nw);
+        my_scan1.set_input(in1);
+        my_scan1.set_operation(add_op, add_op_id);
+        my_scan1.set_scan_type(scan_t);
+
+        //(nw > 0) ? std::cerr << "\nStarting parallel execution...\n" : std::cerr << "\nStarting sequential execution...\n";
+
+        ffTime(START_TIME);
+
+        my_scan1.compute();
+
+        ffTime(STOP_TIME);
+
+        (nw > 0) ? std::cerr << "Tpar(" << nw << ") = " << ffTime(GET_TIME) << " ms\n" :
+                   std::cerr << "Tseq = " << ffTime(GET_TIME) << " ms\n";
+
+    } else if (test == 2) {
+
+        // Measure the multiplication time.
+        auto ta = std::chrono::high_resolution_clock::now();
+        int a = 10, b = 100;
+        int res = a * b;
+        auto tb = std::chrono::high_resolution_clock::now();
+        auto op_time = std::chrono::duration_cast<std::chrono::milliseconds>(tb - ta).count(); // ~ 0 microsecs
+
+        // Time required for sequential execution.
+        std::cerr << "Expected time: Tseq = "
+                  << (vec_size - 1) * (op_time + comp_delay);
+
+        if (nw > 0) {
+            // Time required for parallel execution.
+            std::cerr << " ms  ,  Tpar(" << nw << ") = "
+                      << (((vec_size / nw - 1) * (op_time + comp_delay)) +    // reduce in workers -> O(vec_size/nw)
+                          ((nw - 1) * (op_time + comp_delay)) +               // exclusive scan in distributor -> O(nw)
+                          ((vec_size / nw - 1) * (op_time + comp_delay)))     // scan in workers -> O(vec_size/nw)
+                      << " ms     Real time: ";
+        }
+
+        // ------------------------------------------ TEST MUL ------------------------------------------
+        std::vector<int> in1 = generateIntVector(vec_size, seed);
+        std::function<int(int, int)> mul_op = mul;
+
+        Scan<int> my_scan2(in1, mul_op, mul_op_id, scan_t, nw);
+
+        //(nw > 0) ? std::cerr << "\nStarting parallel execution...\n" : std::cerr << "\nStarting sequential execution...\n";
+
+        ffTime(START_TIME);
+
+        my_scan2.compute();
+
+        ffTime(STOP_TIME);
+
+        (nw > 0) ? std::cerr << "Tpar(" << nw << ") = " << ffTime(GET_TIME) << " ms\n" :
+                   std::cerr << "Tseq = " << ffTime(GET_TIME) << " ms\n";
+
+    } else {
+
+        // Measure the concatenation time.
+        auto ta = std::chrono::high_resolution_clock::now();
+        std::string c("string_one"), d("string_two");
+        std::string str_res = c + d;
+        auto tb = std::chrono::high_resolution_clock::now();
+        auto op_time = std::chrono::duration_cast<std::chrono::milliseconds>(tb - ta).count(); // ~ 7 microsecs
+
+        // Time required for sequential execution.
+        std::cerr << "Expected time: Tseq = "
+                  << (vec_size - 1) * (op_time + comp_delay);
+
+        if (nw > 0) {
+            // Time required for parallel execution.
+            std::cerr << " ms  ,  Tpar(" << nw << ") = "
+                      << (((vec_size / nw - 1) * (op_time + comp_delay)) +       // reduce in workers -> O(vec_size/nw)
+                          ((nw - 1) * (op_time + comp_delay)) +                  // exclusive scan in distributor -> O(nw)
+                          ((vec_size / nw - 1) * (op_time + comp_delay)))        // scan in workers -> O(vec_size/nw)
+                      << " ms     Real time: ";
+        }
+
+
+        // ------------------------------------------ TEST STR ------------------------------------------
+        std::vector<std::string> in2 = generateStringVector(vec_size, seed);
+        std::function<std::string(std::string, std::string)> str_op = str_concat;
+
+        Scan<std::string> my_scan3(in2, str_op, str_conc_id, scan_t, nw);
+
+        //(nw > 0) ? std::cerr << "\nStarting parallel execution...\n" : std::cerr << "\nStarting sequential execution...\n";
+
+        ffTime(START_TIME);
+
+        my_scan3.compute();
+
+        ffTime(STOP_TIME);
+
+        (nw > 0) ? std::cerr << "Tpar(" << nw << ") = " << ffTime(GET_TIME) << " ms\n" :
+                   std::cerr << "Tseq = " << ffTime(GET_TIME) << " ms\n";
+
+    }
+
+    //std::cout << "\nEnd of tests!" << std::endl;
+
+    return 0;
+
+}
+
+std::vector<int> generateIntVector(size_t size, unsigned int seed) {
+    std::mt19937 num_generator(seed);
+    std::uniform_int_distribution<int> dist(0, 50);
+    std::vector<int> v;
+    for (size_t i = 0; i < size; ++i) {
+        v.push_back(dist(num_generator));
+    }
+    return v;
+}
+
+/*
+ * Generate a vector of random strings picking ASCII characters with
+ * decimal encoding in the range [33, 126], as shown in this table
+ * https://en.cppreference.com/w/cpp/language/ascii
+ */
+std::vector<std::string> generateStringVector(size_t size, unsigned int seed) {
+    std::mt19937 num_generator(seed);
+    std::uniform_int_distribution<int> dist(33, 126);
+    std::vector<std::string> v;
+    int str_len = 3;
+    for (size_t i = 0; i < size; ++i) {
+        // create a string composed by the repetition str_len times
+        // of the char encoded by the value of dist(num_generator)
+        // in the ASCII encoding
+        v.emplace_back(std::string(str_len, dist(num_generator)));
+    }
+    return v;
+}
+
+int add (int a, int b) {
+    active_delay(comp_delay);
+    return a + b;
+}
+
+int mul (int a, int b) {
+    active_delay(comp_delay);
+    return a * b;
+}
+
+std::string str_concat (std::string a, std::string b) {
+    active_delay(comp_delay);
+    return a + b;
+}
+
+/*
+ * Add some delay in the function (for tests).
+ */
+void active_delay(int msecs) {
+    auto start = std::chrono::high_resolution_clock::now();
+    bool end = false;
+    while (!end) {
+        auto now = std::chrono::high_resolution_clock::now();
+        auto msec = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
+        end = (msec >= msecs);
+    }
+}
---
+++ final project - parallel prefix/spm_project_ff/scan.h
@@ -0,0 +1,333 @@
+//
+// Created by Alessandra Fais
+// SPM project - MCSN - University of Pisa
+// A.A. 2017/18
+//
+
+#ifndef SPM_PROJECT_SCAN_H
+#define SPM_PROJECT_SCAN_H
+
+#include <chrono>
+#include <condition_variable>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <sstream>
+#include <thread>
+#include <vector>
+
+#include "ff/farm.hpp"
+#include "ff/node.hpp"
+
+using namespace ff;
+
+typedef enum { INCLUSIVE, EXCLUSIVE } scan_type;
+typedef enum { REDUCE, EXSCAN, SCAN } comp_phase;
+
+template <typename T>
+class Scan {
+public:
+    // -------------------------------------- Constructors --------------------------------------
+
+    Scan(std::vector<T>& v, std::function<T (T, T)>& f, T id, scan_type t, size_t par_deg) :
+            scan_t(t), nw(par_deg)
+    {
+        if (v.empty())
+            std::__throw_invalid_argument("Invalid argument: vector is empty.");
+        if (!f)
+            std::__throw_invalid_argument("Invalid argument: operation is null.");
+
+        in = v; // Input vector.
+        out = std::vector<T>(in.size()); // Output vector.
+
+        op = f; // Associative operation.
+        op_id = id; // Identity for the associative operation.
+    }
+
+    Scan(size_t par_deg) : nw(par_deg) { }
+
+    // ---------------------------------------- Methods -----------------------------------------
+
+    /*
+     * Set the input vector.
+     */
+    void set_input(std::vector<T>& v) {
+        if (v.empty())
+            std::__throw_invalid_argument("Invalid argument: vector is empty.");
+        in = v;
+        out = std::vector<T>(in.size());
+    }
+
+    /*
+     * Set the binary associative operation and its identity value.
+     */
+    void set_operation(std::function<T (T, T)>& f, T id) {
+        if (!f)
+            std::__throw_invalid_argument("Invalid argument: operation is null.");
+        op = f;
+        op_id = id;
+    }
+
+    /*
+     * Set the scan type to be inclusive or exclusive.
+     */
+    void set_scan_type(scan_type t) {
+        scan_t = t;
+    }
+
+    /*
+     * Get the input vector.
+     */
+    std::vector<T> get_input() {
+        return in;
+    }
+
+    /*
+     * Get the output vector.
+     */
+    std::vector<T> get_output() {
+        return out;
+    }
+
+    /*
+     * Print the input vector.
+     */
+    void print_in_vec() {
+        std::cout << "Input vector:  " << print(in);
+    }
+
+    /*
+     * Print the output vector.
+     */
+    void print_out_vec() {
+        std::cout << "Output vector:  " << print(out);
+    }
+
+    /*
+     * Compute the scan over the input vector:
+     * if the parallelism degree is nw = 0 then a sequential scan is performed,
+     * otherwise a parallel scan is executed with parallelism degree nw > 0.
+     */
+    void compute() {
+        if (nw == 0) {
+            // Compute sequential scan.
+            (scan_t == EXCLUSIVE) ? seq_exclusive_scan() : seq_inclusive_scan();
+
+        } else if (nw > 0) {
+            ff_Farm<> farm([&]() {
+                std::vector<std::unique_ptr<ff_node>> workers;
+                for(size_t i = 0; i < nw; ++i)
+                    workers.push_back(make_unique<Worker>(in, out, op, op_id, scan_t, i));
+                return workers;
+            } () );
+            Emitter distributor(farm.getlb(), in, op, op_id);
+            farm.add_emitter(distributor);
+            farm.remove_collector();
+            farm.wrap_around();
+
+            if (farm.run_and_wait_end() < 0) std::__throw_runtime_error("Running farm.");
+        }
+    }
+
+private:
+    // --------------------------------------- Subclasses ---------------------------------------
+
+    struct Task {
+        Task(size_t start, size_t stop) : start(start), stop(stop) {}
+
+        const size_t start, stop;   // Extremes of the partition interval.
+        comp_phase cp;              // Computation phase.
+        T reduce_value;             // Reduce result on the partition in[start, stop).
+        size_t worker_id;           // Worker id.
+    };
+
+    // --------------------------------------- Variables ----------------------------------------
+
+    std::vector<T> in;
+    std::vector<T> out;
+
+    std::function<T (T, T)> op;
+    T op_id;
+    scan_type scan_t;
+
+    size_t nw; // (parallelism degree == 0) ? sequential : parallel
+
+    // ------------------------------------ Auxiliary Methods -----------------------------------
+
+    /*
+     * Exclusive sequential scan:
+     * given a sequence [ x0, x1, x2, ... ] calculate output [ id, y0, y1, y2, ... ]
+     * such that y[0] = id and y[i] = y[i - 1] + x[i - 1] for each i > 0.
+     * Complexity is O(n) (n additions for a vector of n elements).
+     */
+    void seq_exclusive_scan() {
+        out.at(0) = op_id;
+        for (size_t i = 1; i < in.size(); ++i) {
+            out.at(i) = op(out.at(i - 1), in.at(i - 1));
+        }
+    }
+
+    /*
+     * Inclusive sequential scan:
+     * given a sequence [ x0, x1, x2, ... ] calculate output [ y0, y1, y2, ... ]
+     * such that y[0] = x[0] and y[i] = y[i - 1] + x[i] for each i > 0.
+     * Complexity is O(n) (n additions for a vector of n elements).
+     */
+    void seq_inclusive_scan() {
+        out.at(0) = op(op_id, in.at(0));
+        for (size_t i = 1; i < in.size(); ++i) {
+            out.at(i) = op(out.at(i - 1), in.at(i));
+        }
+    }
+
+    /*
+     * Generate a string that represents the content of the vector.
+     */
+    std::string print(const std::vector<T>& v) {
+        std::stringstream ss;
+        ss << "{ ";
+        for (auto el : v) {
+            ss << el << " ";
+        }
+        ss << "}\n";
+        return ss.str();
+    }
+
+    // ------------------------------------ Parallel Modules ------------------------------------
+
+    struct Emitter: ff_node {
+
+        Emitter(ff_loadbalancer* const lb,
+                const std::vector<T>& in,
+                const std::function<T (T, T)>& op,
+                const T op_id) :
+                loadbalancer(lb),
+                in(in),
+                op(op),
+                op_id(op_id) { };
+
+        void* svc(void* t) {
+            Task* t_in = reinterpret_cast<Task*>(t);
+            if (t_in == nullptr) {
+                nw = loadbalancer->getNWorkers();
+                th_count = 0;
+                reduce_vec = std::vector<T>(nw);
+                intervals.reserve(nw);
+
+                // Create partitions.
+                const size_t portion = in.size() / nw;
+                ssize_t more = in.size() % nw;
+                size_t start = 0, stop = 0;
+                for (size_t i = 0; i < nw; ++i) {
+                    start = stop;
+                    stop = start + portion + (more > 0 ? 1 : 0);
+                    --more;
+                    intervals.emplace_back(start, stop);
+
+                    // Send task containing start and stop indexes for each partition of the input vector.
+                    auto t = new Task(start, stop);
+                    t->cp = REDUCE;         // Computation phase.
+                    t->reduce_value = op_id;
+                    t->worker_id = i;       // Worker id.
+                    loadbalancer->ff_send_out_to(t, i);
+                }
+
+            } else if (t_in->cp == EXSCAN) {
+                if (++th_count < nw) {
+                    reduce_vec.at(t_in->worker_id) = t_in->reduce_value;
+                }
+                else {
+                    reduce_vec.at(t_in->worker_id) = t_in->reduce_value;
+
+                    std::vector<T> temp(nw);
+                    temp.at(0) = op_id;
+                    for (size_t i = 1; i < nw; ++i) {
+                        temp.at(i) = op(temp.at(i - 1), reduce_vec.at(i - 1));
+                    }
+                    reduce_vec = std::move(temp);
+
+                    // Send to each worker the value to be used in the scan computation on its partition.
+                    for (size_t i = 0; i < nw; ++i) {
+                        auto t = new Task(intervals.at(i).first, intervals.at(i).second);
+                        t->cp = SCAN;
+                        t->reduce_value = reduce_vec.at(i);
+                        t->worker_id = i;
+                        loadbalancer->ff_send_out_to(t, i);
+                    }
+                    loadbalancer->broadcast_task(EOS);
+                }
+
+            }
+            return GO_ON;
+        }
+
+        ff_loadbalancer* loadbalancer;
+        std::vector<T> in;
+        std::function<T (T, T)> op;
+        T op_id;
+
+        size_t nw;
+        size_t th_count;
+        std::vector<std::pair<size_t, size_t>> intervals;
+        std::vector<T> reduce_vec;
+
+    };
+
+    struct Worker : ff_node {
+
+        Worker(const std::vector<T>& in,
+               std::vector<T>& out,
+               const std::function<T (T, T)>& op,
+               const T op_id,
+               const scan_type scan_t,
+               const size_t w_id) :
+                in(in),
+                out(out),
+                op(op),
+                op_id(op_id),
+                scan_t(scan_t),
+                w_id(w_id) { };
+
+        void* svc(void* t) {
+            Task* t_in = reinterpret_cast<Task*>(t);
+
+            if (t_in->cp == REDUCE) {
+                T reduce_value = op_id;
+                std::for_each(std::begin(in) + t_in->start, std::begin(in) + t_in->stop,
+                              [&](const T& val) {
+                                  reduce_value = op(reduce_value, val);
+                              });
+                t_in->cp = EXSCAN;
+                t_in->reduce_value = reduce_value;
+                t_in->worker_id = w_id;
+                return t_in;
+
+            } else if (t_in->cp == SCAN) {
+                if (scan_t == EXCLUSIVE) {
+                    out.at(t_in->start) = t_in->reduce_value;
+                    for (size_t i = t_in->start + 1; i < t_in->stop; ++i) {
+                        out.at(i) = op(out.at(i - 1), in.at(i - 1));
+                    }
+                } else {
+                    out.at(t_in->start) = op(t_in->reduce_value, in.at(t_in->start));
+                    for (size_t i = t_in->start + 1; i < t_in->stop; ++i) {
+                        out.at(i) = op(out.at(i - 1), in.at(i));
+                    }
+                }
+            }
+            return GO_ON;
+        }
+
+        std::vector<T> in;
+        std::vector<T>& out;
+        std::function<T (T, T)> op;
+        T op_id;
+        size_t w_id;
+        scan_type scan_t;
+    };
+
+
+};
+
+#endif //SPM_PROJECT_SCAN_H
